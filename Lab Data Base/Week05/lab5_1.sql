d7dbd0f061ddaf2664cc2336a0e3231586575de7
poonchub <141898614+poonchub@users.noreply.github.com>
2024-04-06 18:41:45 +0700

    6/4/24

--- Lab Data Base/Week05/lab5_1.sql
+++ Lab Data Base/Week05/lab5_1.sql
@@ -6,12 +6,12 @@ select officer.FIRST_NAME, individual.FIRST_NAME
 from lab_week6.officer inner join lab_week6.individual inner join lab_week6.customer
 on customer.CUST_ID = officer.CUST_ID and officer.CUST_ID = individual.CUST_ID;
 
-SELECT 
+select 
     e1.EMP_ID,
     e1.FIRST_NAME,
     e1.LAST_NAME,
     e2.FIRST_NAME,
     e2.LAST_NAME
-FROM employee e1 LEFT JOIN employee e2
-ON e1.SUPERIOR_EMP_ID = e2.EMP_ID
-LIMIT 5;
+from employee e1 left join employee e2
+on e1.SUPERIOR_EMP_ID = e2.EMP_ID
+limit 5;
